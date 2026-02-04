753a2b6546d34fca8703d60497c008113352dffd
Rogan <quangenw@gmail.com>
2026-02-04 16:14:39 +0800

    rename multiple files for improved organization and clarity

--- apps/demo-app/src/main/java/io/softa/app/demo/service/impl/DeptInfoServiceImpl.java
+++ apps/demo-app/src/main/java/io/softa/app/demo/service/impl/DeptInfoServiceImpl.java
@@ -1,9 +1,13 @@
 package io.softa.app.demo.service.impl;
 
 import org.springframework.stereotype.Service;
-import io.softa.framework.orm.service.impl.EntityServiceImpl;
+
 import io.softa.app.demo.entity.DeptInfo;
 import io.softa.app.demo.service.DeptInfoService;
+import io.softa.framework.orm.domain.Filters;
+import io.softa.framework.orm.domain.FlexQuery;
+import io.softa.framework.orm.domain.Orders;
+import io.softa.framework.orm.service.impl.EntityServiceImpl;
 
 /**
  * DeptInfo Model Service Implementation
@@ -11,4 +15,11 @@ import io.softa.app.demo.service.DeptInfoService;
 @Service
 public class DeptInfoServiceImpl extends EntityServiceImpl<DeptInfo, Long> implements DeptInfoService {
 
+    public DeptInfo getDeptInfoByCode(String code) {
+        Filters filters = new Filters().eq("code", code);
+        FlexQuery flexQuery = new FlexQuery().where(filters)
+                .select(DeptInfo::getName, DeptInfo::getCode, DeptInfo::getDescription, DeptInfo::getActive)
+                .orderBy(Orders.ofAsc(DeptInfo::getId).addAsc(DeptInfo::getName));
+        return this.searchOne(flexQuery).orElse(null);
+    }
 }
